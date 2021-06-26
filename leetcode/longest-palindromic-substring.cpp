ecaefed30377a3ba2c4d711db914d41a3947325f
khalilalquraan <khalilalquraan4@gmail.com>
2021-06-27 02:24:37 +0300

    edit solution

--- leetcode/longest-palindromic-substring.cpp
+++ leetcode/longest-palindromic-substring.cpp
@@ -4,41 +4,33 @@
 */
 
 class Solution {
-public:
-    string longestPalindrome(string s) {
-        int n = s.size();
-        int len = 1;
-        int ansl = 0 , ansr = 0;
-        for(int i = 0 ; i < n ; i++) {
-            int l = i - 1 , r = i + 1;
-            while(l >= 0 && r < n) {
-                if(s[l] == s[r]) {
-                    if(len < r - l + 1) {
-                        len = r - l + 1;
-                        ansl = l;
-                        ansr = r;
-                    }
-                    l--;
-                    r++;
-                } else {
-                    break;
-                }
-            }
-            l = i , r = i + 1;
-            while(l >= 0 && r < n) {
-                if(s[l] == s[r]) {
-                    if(len < r - l + 1) {
-                        len = r - l + 1;
-                        ansl = l;
-                        ansr = r;
-                    }
-                    l--;
-                    r++;
-                } else {
-                    break;
-                }
-            }
+ public:
+  string longestPalindrome(string s) {
+    int n = s.size();
+    int len = 1;
+    int ansl = 0, ansr = 0;
+    for (int i = 0; i < n; i++) {
+      int l = i - 1, r = i + 1;
+      while (l >= 0 && r < n && s[l] == s[r]) {
+        if (len < r - l + 1) {
+          len = r - l + 1;
+          ansl = l;
+          ansr = r;
         }
-        return s.substr(ansl,len);
+        l--;
+        r++;
+      }
+      l = i, r = i + 1;
+      while (l >= 0 && r < n && s[l] == s[r]) {
+        if (len < r - l + 1) {
+          len = r - l + 1;
+          ansl = l;
+          ansr = r;
+        }
+        l--;
+        r++;
+      }
     }
+    return s.substr(ansl, len);
+  }
 };
